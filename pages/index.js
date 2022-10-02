d1290a537a56be7880e5216be884b8918afb9eeb
myon1664 <113237050+myon1664@users.noreply.github.com>
2022-10-02 11:17:04 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,10 +14,12 @@ export default function Home() {
       <main>
         <h2><img src="https://souldimension.org/wp-content/uploads/2022/10/Universal-Breathworkers.jpg" alt="Universal Breathworkers" class="responsive" /></h2>
         <Header title="Welcome to the Universal Breathworkers!" />
-        <p><strong>Learn a simple and powerful breathing technique</strong> to increase your physical, mental, emotional and spiritual health &amp; well-being.</p>
+        <p>Learn a simple and powerful breathing technique to increase your physical, mental, emotional and spiritual health &amp; well-being.</p>
         <p>Join us to learn and practice the <strong><a href="https://souldimension.org/joy-of-breathing/" target="_blank" >Joy of Breathing</a></strong>.</p>
-        <h2><strong>EVERY WEDNESDAY WE BREATHE TOGETHER AND GENERATE A GLOBAL HEALING CURRENT</strong></h2>
-        <p><strong><a href="https://souldimension.org/breathe/" target="_blank">Join the Zoom Meeting</a></strong>, breathe and heal with us.</p>
+        <h2><strong>EVERY WEDNESDAY</strong></h2>
+        <h2><strong>WE BREATHE TOGETHER</strong></h2>
+        <h2><strong>AND GENERATE A GLOBAL HEALING CURRENT</strong></h2>
+        <p><a href="https://souldimension.org/breathe/" target="_blank">Join the Zoom Meeting</a>, breathe and heal with us.</p>
         <p><em>Organized by <a href="https://souldimension.org/" target="_blank">Soul Dimension</a></em></p>
       </main>
 
