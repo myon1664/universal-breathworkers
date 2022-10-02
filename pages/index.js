d12c41e857827de2f46846677a6239e800be6ae0
myon1664 <113237050+myon1664@users.noreply.github.com>
2022-10-02 11:18:49 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main>
         <h2><img src="https://souldimension.org/wp-content/uploads/2022/10/Universal-Breathworkers.jpg" alt="Universal Breathworkers" class="responsive" /></h2>
         <Header title="Welcome to the Universal Breathworkers!" />
-        <p>Learn a simple and powerful breathing technique to increase your physical, mental, emotional and spiritual health &amp; well-being.</p>
+        <h3>Learn a simple and powerful breathing technique to increase your physical, mental, emotional and spiritual health &amp; well-being.</h3>
         <p>Join us to learn and practice the <strong><a href="https://souldimension.org/joy-of-breathing/" target="_blank" >Joy of Breathing</a></strong>.</p>
         <h2><strong>EVERY WEDNESDAY</strong></h2>
         <h2><strong>WE BREATHE TOGETHER</strong></h2>
