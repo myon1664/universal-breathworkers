0998d1b03164966839057d117e734bb62bae859f
myon1664 <113237050+myon1664@users.noreply.github.com>
2022-10-02 10:04:02 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ export default function Home() {
     <div className="container">
     
       <Head>
-        //<center><h2><img src="https://souldimension.org/wp-content/uploads/2022/10/Universal-Breathworkers.jpg" alt="" width="1200" height="600" /></h2></center>
         <title>Welcome to the Universal Breathworkers!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
