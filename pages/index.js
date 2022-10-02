1711904cf5f89701aca07c63f0c0c9d3952f6902
myon1664 <113237050+myon1664@users.noreply.github.com>
2022-10-02 09:57:38 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,9 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to the Universal Breathworkers!" />
-        <center><p><strong>Learn a simple and powerful breathing technique</strong> to increase your physical, mental, emotional and spiritual health &amp; well-being.</p><br>
-        <p>Join us to learn and practice the <strong><a href="https://souldimension.org/joy-of-breathing/" target="_blank" rel="nofollow ugc">Joy of Breathing</a></strong>.</p><br>
-        <h2><strong>EVERY WEDNESDAY WE BREATHE TOGETHER AND GENERATE A GLOBAL HEALING CURRENT</strong></h2><br>
+        <center><p><strong>Learn a simple and powerful breathing technique</strong> to increase your physical, mental, emotional and spiritual health &amp; well-being.</p>
+        <p>Join us to learn and practice the <strong><a href="https://souldimension.org/joy-of-breathing/" target="_blank" rel="nofollow ugc">Joy of Breathing</a></strong>.</p>
+        <h2><strong>EVERY WEDNESDAY WE BREATHE TOGETHER AND GENERATE A GLOBAL HEALING CURRENT</strong></h2>
         <p><strong><a href="https://souldimension.org/breathe/" target="_blank" rel="nofollow ugc">Join the Zoom Meeting</a></strong>, breathe and heal with us.</p>
         <p><em>Organized by <a href="https://souldimension.org/" target="_blank" rel="nofollow ugc">Soul Dimension</a></em></p></center>
       </main>
